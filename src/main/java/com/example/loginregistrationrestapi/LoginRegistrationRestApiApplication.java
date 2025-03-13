766a3ea3aceb3933a72dee3ecf49078f151dee84
SamiraKetabifard <samiraketabi.fard@gmail.com>
2025-03-13 22:23:52 +0330

    first commit

---
+++ src/main/java/com/example/loginregistrationrestapi/LoginRegistrationRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.loginregistrationrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginRegistrationRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginRegistrationRestApiApplication.class, args);
+    }
+
+}
